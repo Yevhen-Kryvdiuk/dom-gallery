f95ded649ec3ebd300be5903ef0ed517b0059b44
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-05-20 15:14:38 +0300

    took into account the comments

--- src/script/index.js
+++ src/script/index.js
@@ -1,21 +1,13 @@
 const large = document.querySelector('.large');
 const bottomImg = document.querySelector('.bottom-img');
 
-bottomImg.onclick = function (event) {
-    let  target = event.target;
-    console.log(target);
-    while (target !== this) {
-        if (target.nodeName === 'A') {
-            return changeLargeImg(target.href, target.title);
-        }
-
-         target = target.parentNode;
-    }
-
-};
+bottomImg.addEventListener('click',  function (event) {
+    event.preventDefault();
+    const  target = event.target.closest('a');
+    return changeLargeImg(target.href, target.title);
+});
 
 function changeLargeImg(href, title) {
     large.src = href;
     large.alt = title;
-    return false;
 }
